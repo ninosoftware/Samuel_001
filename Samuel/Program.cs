1164a86bc01a8975e54c47e6ff8fa2d4f0aebf8d
miguel camacho <ninosoftware@outlook.com>
2015-09-16 00:39:33 +0200

    ok

---
+++ Samuel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of command line parameters = {0}", args.Length);
+            int paramnbr = 1;
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Number of parameters insufficient, inadequate.");
+            }
+            else
+            {
+                foreach (string s in args)
+                {
+                    Console.WriteLine("Param " + paramnbr.ToString() + ": " + s);
+                    paramnbr++;
+                }
+            }
+
+        }
+    }
+}
